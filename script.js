3785ab0f92655bfb3470e7726528408a8a11bf2e
nicomaco1 <ng.t@outlook.com>
2025-04-21 13:09:50 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function tellCatJoke() {
+    const joke = "Why was the cat sitting on the computer? 💻😺";
+    const punchline = "Because it wanted to keep an eye on the mouse! 🐭🤣";
+    alert(joke);
+    setTimeout(() => alert(punchline), 1500);
+}
+function openLinkWithMessage(message, url) {
+    alert(message);
+    window.open(url, '_blank');
+}
+
+function tellCatJoke() {
+    const joke = "Why was the cat sitting on the computer? 💻😺";
+    const punchline = "Because it wanted to keep an eye on the mouse! 🐭🤣";
+    alert(joke);
+    setTimeout(() => alert(punchline), 1500);
+}
